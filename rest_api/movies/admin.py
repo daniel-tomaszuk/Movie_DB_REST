ed6b351b2b1b1119f10b5fd44e12245d5c81c3f7
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-07-03 15:22:34 +0200

    add models into admin panel

--- rest_api/movies/admin.py
+++ rest_api/movies/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ("name")
+
+
+@admin.register(Movie)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ("title", "description", "director", "year", "actors")
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ("person_role", "movie_role", "role")
+
+
